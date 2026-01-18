ba34db9e571aa91e9ecdfea910dfaf9e8d14922e
sahil arora <sahilarora@sahils-MacBook-Air.local>
2026-01-18 14:23:24 +0530

    improved-version

--- json-formatter-ui/src/app/json-formatter/json-formatter.component.ts
+++ json-formatter-ui/src/app/json-formatter/json-formatter.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { JsonFormatterService } from '../json-formatter/json-formatter.component.service';
+import JSON5 from 'json5';
 
 @Component({
   selector: 'app-json-formatter',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './json-formatter.component.html',
+  styleUrls: ['./json-formatter.component.css']
+
 })
 export class JsonFormatterComponent {
   inputJson = '';
@@ -17,12 +20,39 @@ export class JsonFormatterComponent {
   constructor(private service: JsonFormatterService) {}
 
   formatJson() {
-    this.error = '';
-    this.outputJson = '';
+    if (!this.inputJson || !this.inputJson.trim()) {
+      return;
+    }
+  
+    try {
+      const parsed = JSON5.parse(this.inputJson);
+      this.inputJson = JSON.stringify(parsed, null, 2);
 
-    this.service.format(this.inputJson).subscribe({
-      next: res => this.outputJson = res.formatted,
-      error: err => this.error = err.error?.error || 'Invalid JSON'
-    });
+    } catch(error) {
+      console.log('error', error);
+      this.inputJson = this.looseFormat(this.inputJson);
+
+      // If parsing fails, do nothing
+      // User keeps original input
+    }
   }
+  
+  looseFormat(input: string): string {
+    let indent = 0;
+    const INDENT = 2;
+  
+    return input
+      .replace(/([{[])/g, '$1\n')
+      .replace(/([}\]])/g, '\n$1')
+      .replace(/,/g, ',\n')
+      .split('\n')
+      .map(line => {
+        if (line.match(/^[}\]]/)) indent -= INDENT;
+        const padded = ' '.repeat(Math.max(indent, 0)) + line.trim();
+        if (line.match(/[{[]$/)) indent += INDENT;
+        return padded;
+      })
+      .join('\n');
+  }
+  
 }
